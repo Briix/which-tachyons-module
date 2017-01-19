452c488cd755bc62945c3a2741de89a99fc53d40
Simon Brix <vdb.simon@gmail.com>
2017-01-19 13:37:41 +0100

    complete implementation of generate script

--- generate.js
+++ generate.js
@@ -1,12 +1,23 @@
 const tachyonsModules = require('tachyons-modules')
+const fs = require('fs')
+const cssResolve = require('style-resolve').sync
+const extract = require('extract-from-css')
+
 const moduleBlacklist = [
   'react-native-style-tachyons',
   'tachyons-base',
   'tachyons-build-css',
   'tachyons-debug',
+  'tachyons-debug-children',
+  'tachyons-debug-grid',
+  'tachyons-x-ray',
   'tachyons-display-verbose',
   'tachyons-verbose',
-  'tachyons-webpack'
+  'tachyons-webpack',
+  'img',
+  'jekyll-tachyons',
+  'tachyons-and-react',
+  'tachyons-custom'
 ]
 
 const inBlacklist = value =>
@@ -20,10 +31,39 @@ const log = item => {
   return item
 }
 
+const resolve = value =>
+  cssResolve(value, { basedir: __dirname })
+
+const readFile = file =>
+  fs.readFileSync(resolve(file), 'utf8')
+
 const reject = arr =>
   arr.filter(inBlacklist)
+
+const generate = arr =>
+  arr.map((x) => ({
+    name: x,
+    values: extract.extractClasses(readFile(x))
+  }))
+
+const toJSON = data =>
+  JSON.stringify(data, null, 2)
+
+const addExport = json =>
+  `module.exports = ${json}`
+
+const writeFile = data =>
+  new Promise((res, rej) => {
+    fs.writeFile('data.js', addExport(data), (err, result) => {
+      if (err) { rej(err) }
+      res(result);
+    })
+  })
 
 tachyonsModules()
   .then(returnNames)
   .then(reject)
-  .then(log)
+  .then(generate)
+  .then(toJSON)
+  .then(writeFile)
+  .catch(e => console.log(e))
